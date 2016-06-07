54d77a1dc4cf8cfa21fcc7c6217e826a780e263a
Leo Osvald <leo.osvald@gmail.com>
2016-06-06 22:20:57 -0700

    Show line numbers in change-context.sh and suppress stderr of sloc.py

--- scripts/eval/change-context.sh
+++ scripts/eval/change-context.sh
@@ -3,5 +3,5 @@
 scriptdir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 (( $# < 1 )) && echo "Usage: [N=3] $0 PERL_REGEX [sloc.py_FLAGS]" >&2 && exit 1
 re="$1"; shift
-"$scriptdir/sloc.py" -v "$@" | grep -v '^$' | grep -P "$re" | \
-    sed -e 's:\([][]\|[*.]\):\\\1:g' | grep -C${N:-0} -f - -R src/library/scala
+"$scriptdir/sloc.py" -v "$@" 2>/dev/null | grep -v '^$' | grep -P "$re" | \
+    sed -e 's:\([][]\|[*.]\):\\\1:g' | grep -C${N:-0} -f - -nR src/library/scala
